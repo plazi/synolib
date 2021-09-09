ec94cc3a026928b58adce730d07ae9e4497aa1c4
Reto Gmür <reto@factsmission.com>
2021-09-09 12:29:15 +0200

    taxon as command line arg

--- main.ts
+++ main.ts
@@ -6,32 +6,49 @@ import * as Colors from "https://deno.land/std/fmt/colors.ts";
 const sparqlEndpoint = new SparqlEndpoint(
   "https://treatment.ld.plazi.org/sparql",
 );
-const taxonName = "Sadayoshia acroporae";
+const taxonName = Deno.args.length > 0
+  ? Deno.args.join(" ")
+  : "Sadayoshia acroporae";
 const synoGroup = new SynoGroup(sparqlEndpoint, taxonName);
 
 console.log(Colors.blue(`Synonym Group For ${taxonName}`));
 for await (const synonym of synoGroup) {
-
   console.log(Colors.red(` * Found synonym: ${synonym.taxonConceptUri}`));
 
   (async () => {
     for await (const justification of synonym.justifications) {
-      console.log(Colors.magenta(` - Found justification for ${synonym.taxonConceptUri}: ${justification}`));
+      console.log(
+        Colors.magenta(
+          ` - Found justification for ${synonym.taxonConceptUri}: ${justification}`,
+        ),
+      );
     }
   })();
   (async () => {
     for await (const treatment of synonym.treatments!.aug) {
-      console.log(Colors.gray(` - Found augmenting treatment for ${synonym.taxonConceptUri}: ${treatment.url}`));
+      console.log(
+        Colors.gray(
+          ` - Found augmenting treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
+        ),
+      );
     }
   })();
   (async () => {
     for await (const treatment of synonym.treatments.def) {
-      console.log(Colors.gray(` - Found defining treatment for ${synonym.taxonConceptUri}: ${treatment.url}`));
+      console.log(
+        Colors.gray(
+          ` - Found defining treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
+        ),
+      );
     }
   })();
   (async () => {
     for await (const treatment of synonym.treatments.dpr) {
-      console.log(Colors.gray(` - Found deprecating treatment for ${synonym.taxonConceptUri}: ${treatment.url}`));
+      console.log(
+        Colors.gray(
+          ` - Found deprecating treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
+        ),
+      );
     }
   })();
 }
