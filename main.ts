beec196e4a93c58645cfac1a77f827917641ce2d
nleanba <25827850+nleanba@users.noreply.github.com>
2024-02-12 15:23:04 +0100

    abort requests after synonym group has been aborted should imprive perfomance after an abort – not clogging up future requests

--- main.ts
+++ main.ts
@@ -11,44 +11,53 @@ const taxonName = Deno.args.length > 0
   : "Sadayoshia acroporae";
 const synoGroup = new SynoGroup(sparqlEndpoint, taxonName);
 
-console.log(Colors.blue(`Synonym Group For ${taxonName}`));
-for await (const synonym of synoGroup) {
-  console.log(Colors.red(` * Found synonym: ${synonym.taxonConceptUri}`));
+setTimeout(() => {
+  console.log("aborting after 1s");
+  synoGroup.abort();
+}, 1000);
 
-  (async () => {
-    for await (const justification of synonym.justifications) {
-      console.log(
-        Colors.magenta(
-          ` - Found justification for ${synonym.taxonConceptUri}: ${justification}`,
-        ),
-      );
-    }
-  })();
-  (async () => {
-    for await (const treatment of synonym.treatments!.aug) {
-      console.log(
-        Colors.gray(
-          ` - Found augmenting treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
-        ),
-      );
-    }
-  })();
-  (async () => {
-    for await (const treatment of synonym.treatments.def) {
-      console.log(
-        Colors.gray(
-          ` - Found defining treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
-        ),
-      );
-    }
-  })();
-  (async () => {
-    for await (const treatment of synonym.treatments.dpr) {
-      console.log(
-        Colors.gray(
-          ` - Found deprecating treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
-        ),
-      );
-    }
-  })();
+console.log(Colors.blue(`Synonym Group For ${taxonName}`));
+try {
+  for await (const synonym of synoGroup) {
+    console.log(Colors.red(` * Found synonym: ${synonym.taxonConceptUri}`));
+
+    (async () => {
+      for await (const justification of synonym.justifications) {
+        console.log(
+          Colors.magenta(
+            ` - Found justification for ${synonym.taxonConceptUri}: ${justification}`,
+          ),
+        );
+      }
+    })();
+    (async () => {
+      for await (const treatment of synonym.treatments!.aug) {
+        console.log(
+          Colors.gray(
+            ` - Found augmenting treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
+          ),
+        );
+      }
+    })();
+    (async () => {
+      for await (const treatment of synonym.treatments.def) {
+        console.log(
+          Colors.gray(
+            ` - Found defining treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
+          ),
+        );
+      }
+    })();
+    (async () => {
+      for await (const treatment of synonym.treatments.dpr) {
+        console.log(
+          Colors.gray(
+            ` - Found deprecating treatment for ${synonym.taxonConceptUri}: ${treatment.url}`,
+          ),
+        );
+      }
+    })();
+  }
+} catch (error) {
+  console.error(Colors.red(error + ""));
 }
