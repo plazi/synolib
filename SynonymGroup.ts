dbac3144971892f541d3869332dc2aba42f393f7
nleanba <25827850+nleanba@users.noreply.github.com>
2024-03-20 16:33:52 +0100

    add title to treatments in treatmentset

--- SynonymGroup.ts
+++ SynonymGroup.ts
@@ -29,6 +29,7 @@ export type Treatment = {
   materialCitations: Promise<MaterialCitation[]>;
   date?: number;
   creators?: string;
+  title?: string;
 };
 
 interface TreatmentJustification extends Justification {
@@ -561,17 +562,18 @@ GROUP BY ?tc ?tn ?treat ?date`;
         const query = `PREFIX treat: <${treat}>
     PREFIX dc: <http://purl.org/dc/elements/1.1/>
     PREFIX dwc: <http://rs.tdwg.org/dwc/terms/>
-    SELECT DISTINCT ?treat ?how ?date (group_concat(DISTINCT ?c;separator="; ") as ?creators)
+    SELECT DISTINCT ?treat ?how ?date ?title (group_concat(DISTINCT ?c;separator="; ") as ?creators)
     WHERE {
       ?treat (treat:definesTaxonConcept|treat:augmentsTaxonConcept|treat:deprecates) <${uri}> ;
               ?how <${uri}> ;
               dc:creator ?c .
+      OPTIONAL { ?treat dc:title ?title }
       OPTIONAL {
         ?treat treat:publishedIn ?pub .
         ?pub dc:date ?date .
       }
     }
-    GROUP BY ?treat ?how ?date`;
+    GROUP BY ?treat ?how ?date ?title`;
         // console.info('%cREQ', 'background: red; font-weight: bold; color: white;', `getTreatments('${uri}')`)
 
         if (fetchInit.signal.aborted) return new Promise((r) => r());
@@ -584,6 +586,7 @@ GROUP BY ?tc ?tn ?treat ?date`;
                 date: t.date ? parseInt(t.date.value, 10) : undefined,
                 creators: t.creators.value,
                 materialCitations: getMaterialCitations(t.treat.value),
+                title: t.title?.value,
               };
               switch (t.how.value) {
                 case treat + "definesTaxonConcept":
