035d6b42b79771b406d675a37e2d81fe9c965627
nleanba <25827850+nleanba@users.noreply.github.com>
2024-03-26 15:32:25 +0000

    Deploying to gh-pages from @ plazi/synolib@4069f7de7fc64eb94cce1d047e2630091285a6a1 🚀

--- synonym-group.js
+++ synonym-group.js
@@ -218,7 +218,7 @@ GROUP BY ?date ?title ?mc`;
                     materialCitations: []
                 };
                 json.results.bindings.forEach((t)=>{
-                    if (!t.mc || !t.catalogNumber) return;
+                    if (!t.mc || !t.catalogNumbers?.value) return;
                     const mc = {
                         "catalogNumber": t.catalogNumbers.value,
                         "collectionCode": t.collectionCodes?.value,
