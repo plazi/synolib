74f4c757f3d7281638370fbf605799c1df050152
nleanba <25827850+nleanba@users.noreply.github.com>
2024-07-29 19:37:47 +0200

    moved endpoint to own file

---
+++ SparqlEndpoint.ts
@@ -0,0 +1,73 @@
+async function sleep(ms: number): Promise<void> {
+  const p = new Promise<void>((resolve) => {
+    setTimeout(resolve, ms);
+  });
+  return await p;
+}
+
+export type SparqlJson = {
+  head: {
+    vars: string[];
+  };
+  results: {
+    bindings: {
+      [key: string]: { type: string; value: string; "xml:lang"?: string };
+    }[];
+  };
+};
+
+/**
+ * Represents a remote sparql endpoint and provides a uniform way to run queries.
+ */
+export class SparqlEndpoint {
+  constructor(private sparqlEnpointUri: string) {
+  }
+
+  /**
+   * Run a query against the sparql endpoint
+   *
+   * It automatically retries up to 10 times on fetch errors, waiting 50ms on the first retry and doupling the wait each time.
+   * Retries are logged to the console (`console.warn`)
+   *
+   * @throws In case of non-ok response status codes or if fetch failed 10 times.
+   * @param query The sparql query to run against the endpoint
+   * @param fetchOptions Additional options for the `fetch` request
+   * @param _reason (Currently ignored, used internally for debugging purposes)
+   * @returns Results of the query
+   */
+  async getSparqlResultSet(
+    query: string,
+    fetchOptions: RequestInit = {},
+    _reason = "",
+  ) {
+    fetchOptions.headers = fetchOptions.headers || {};
+    (fetchOptions.headers as Record<string, string>)["Accept"] =
+      "application/sparql-results+json";
+    let retryCount = 0;
+    const sendRequest = async (): Promise<SparqlJson> => {
+      try {
+        // console.info(`SPARQL ${_reason} (${retryCount + 1})`);
+        const response = await fetch(
+          this.sparqlEnpointUri + "?query=" + encodeURIComponent(query),
+          fetchOptions,
+        );
+        if (!response.ok) {
+          throw new Error("Response not ok. Status " + response.status);
+        }
+        return await response.json();
+      } catch (error) {
+        if (fetchOptions.signal?.aborted) {
+          throw error;
+        } else if (retryCount < 10) {
+          const wait = 50 * (1 << retryCount++);
+          console.warn(`!! Fetch Error. Retrying in ${wait}ms (${retryCount})`);
+          await sleep(wait);
+          return await sendRequest();
+        }
+        console.warn("!! Fetch Error:", query, "\n---\n", error);
+        throw error;
+      }
+    };
+    return await sendRequest();
+  }
+}
--- SynonymGroup.ts
+++ SynonymGroup.ts
@@ -2,6 +2,10 @@
 import { JustificationSet } from "./JustificationSet.ts";
 // @ts-ignore: Import unneccesary for typings, will collate .d.ts files
 export * from "./JustificationSet.ts";
+// @ts-ignore: Import unneccesary for typings, will collate .d.ts files
+import { SparqlEndpoint, SparqlJson } from "./SparqlEndpoint.ts";
+// @ts-ignore: Import unneccesary for typings, will collate .d.ts files
+export * from "./SparqlEndpoint.ts";
 
 export type MaterialCitation = {
   "catalogNumber": string;
@@ -85,79 +89,6 @@ export type JustifiedSynonym = {
   loading: boolean;
 };
 
-async function sleep(ms: number): Promise<void> {
-  const p = new Promise<void>((resolve) => {
-    setTimeout(resolve, ms);
-  });
-  return await p;
-}
-
-type SparqlJson = {
-  head: {
-    vars: string[];
-  };
-  results: {
-    bindings: {
-      [key: string]: { type: string; value: string; "xml:lang"?: string };
-    }[];
-  };
-};
-
-/**
- * Represents a remote sparql endpoint and provides a uniform way to run queries.
- */
-export class SparqlEndpoint {
-  constructor(private sparqlEnpointUri: string) {
-  }
-
-  /**
-   * Run a query against the sparql endpoint
-   *
-   * It automatically retries up to 10 times on fetch errors, waiting 50ms on the first retry and doupling the wait each time.
-   * Retries are logged to the console (`console.warn`)
-   *
-   * @throws In case of non-ok response status codes or if fetch failed 10 times.
-   * @param query The sparql query to run against the endpoint
-   * @param fetchOptions Additional options for the `fetch` request
-   * @param _reason (Currently ignored, used internally for debugging purposes)
-   * @returns Results of the query
-   */
-  async getSparqlResultSet(
-    query: string,
-    fetchOptions: RequestInit = {},
-    _reason = "",
-  ) {
-    fetchOptions.headers = fetchOptions.headers || {};
-    (fetchOptions.headers as Record<string, string>)["Accept"] =
-      "application/sparql-results+json";
-    let retryCount = 0;
-    const sendRequest = async (): Promise<SparqlJson> => {
-      try {
-        // console.info(`SPARQL ${_reason} (${retryCount + 1})`);
-        const response = await fetch(
-          this.sparqlEnpointUri + "?query=" + encodeURIComponent(query),
-          fetchOptions,
-        );
-        if (!response.ok) {
-          throw new Error("Response not ok. Status " + response.status);
-        }
-        return await response.json();
-      } catch (error) {
-        if (fetchOptions.signal?.aborted) {
-          throw error;
-        } else if (retryCount < 10) {
-          const wait = 50 * (1 << retryCount++);
-          console.warn(`!! Fetch Error. Retrying in ${wait}ms (${retryCount})`);
-          await sleep(wait);
-          return await sendRequest();
-        }
-        console.warn("!! Fetch Error:", query, "\n---\n", error);
-        throw error;
-      }
-    };
-    return await sendRequest();
-  }
-}
 
 export default class SynonymGroup implements AsyncIterable<JustifiedSynonym> {
   justifiedArray: JustifiedSynonym[] = [];
